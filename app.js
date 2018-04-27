c6a3a3ac46b7fa2abbf0c2983896c7b5498b52a4
Ross Daniel Kiser <kiserr10@gmail.com>
2018-04-27 12:49:57 -0600

    added main player with sprite sheet and added movement

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+
 let platforms;
+let player;
+let cursors;
 
 function preload(){
     this.load.image('sky', './assets/sky.png');
@@ -29,7 +32,9 @@ function preload(){
         { frameWidth: 32, frameHeight: 48 }
     );
 }
+
 function create(){
+    cursors = this.input.keyboard.createCursorKeys();
     this.add.image(400, 300, 'sky');
 
     platforms = this.physics.add.staticGroup(); 
@@ -37,6 +42,7 @@ function create(){
     //applies a STATIC physics group to the variable platforms...these are STATIC bodies VS DYNAMIC
     //A Physics Group will automatically create physics enabled children, saving you some leg-work in the process.
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    //The call to refreshBody() is required because we have scaled a static physics body, so we have to tell the physics world about the changes we made.
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
@@ -45,8 +51,47 @@ function create(){
     //the game object is postioned center by default (dimensions: 800 x 600)
     //you can also use set origin to change this
     //images displayed in order that they are preloaded!!!
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.body.setGravityY(300);
+
+    this.physics.add.collider(player, platforms); //collide detection between the two Game Objects passed in!
+    
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+    
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update(){
-
+    if(cursors.left.isDown){
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    } else if(cursors.right.isDown){
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+    if(cursors.up.isDown && player.body.touching.down){ 
+        //checks to see if the player is touching the ground/floor 
+        //player shouldnt be able to jump from mid air
+        player.setVelocityY(-330);
+    }
 }
