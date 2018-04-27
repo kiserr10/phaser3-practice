9de7f4ad141406890858d8b1674cbce8689de981
Ross Daniel Kiser <kiserr10@gmail.com>
2018-04-27 12:09:28 -0600

    platforms in place for game

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const config = {
     type: Phaser.AUTO, //this is the rendering context, auto tries to use WebGL fallback is canvas
     width: 800, //game resolution
-    height: 600, //game resolution
+    height: 600,//game resolution
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    }, 
     scene: {
         preload: preload,
         create: create,
@@ -10,6 +17,7 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let platforms;
 
 function preload(){
     this.load.image('sky', './assets/sky.png');
@@ -23,12 +31,17 @@ function preload(){
 }
 function create(){
     this.add.image(400, 300, 'sky');
-    this.add.image(0, 0, 'bomb').setOrigin(0, 0);
-    this.add.image(200, 300, 'star').setOrigin(0, 0);
+
+    platforms = this.physics.add.staticGroup(); //call to Arcade Physics System...must add to game config
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
     //without setOrigin, image is half out of view!!!!
     // x and y coordinates of the image loaded
     //the game object is postioned center by default (dimensions: 800 x 600)
     //you can also use set origin to change this
+    //images displayed in order that they are preloaded!!!
 }
 
 function update(){
